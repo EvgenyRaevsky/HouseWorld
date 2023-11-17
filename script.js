23098c574e72519063912826cd48514b826cf1df
Evgeny <zhenya.raewskiy@yandex.ru>
2023-11-18 01:10:03 +0300

    header block layout, error correction, image replacement

--- script.js
+++ script.js
@@ -13,3 +13,36 @@ for (let el of droplist) {
         }
     )
 }
+
+const menu = document.querySelector('.nav__menu')
+const dropDownMenu = document.querySelector('.drop-down-menu')
+const headerSection = document.querySelectorAll('.header-section')
+
+dropDownMenu.addEventListener('click', () => {
+    if (menu.classList.contains('nav__menu_active')) {
+        dropDownMenu.classList.add('drop-down-menu_turn-right')
+        menu.classList.add('nav__menu_enter')
+        headerSection[0].classList.add('header-section_shift-up')
+        setTimeout(() => {
+            menu.classList.remove('nav__menu_enter')
+            dropDownMenu.classList.remove('drop-down-menu_turn-right')
+            menu.classList.remove('nav__menu_active')
+            dropDownMenu.classList.remove('drop-down-menu_active')
+            headerSection[0].classList.remove('header-section_shift-up')
+            headerSection[0].classList.remove('header-section_down')
+        }, 490)
+    }
+    else {
+        dropDownMenu.classList.add('drop-down-menu_turn-left')
+        menu.classList.add('nav__menu_exit')
+        headerSection[0].classList.add('header-section_shift-down')
+        setTimeout(() => {
+            menu.classList.remove('nav__menu_exit')
+            dropDownMenu.classList.remove('drop-down-menu_turn-left')
+            menu.classList.add('nav__menu_active')
+            dropDownMenu.classList.add('drop-down-menu_active')
+            headerSection[0].classList.remove('header-section_shift-down')
+            headerSection[0].classList.add('header-section_down')
+        }, 490)
+    }
+})
